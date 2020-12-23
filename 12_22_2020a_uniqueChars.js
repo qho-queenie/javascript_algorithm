6a2fcb9d626e7e7901b0f2cf4ec693c09a46cbdd
Queenie Ho <qho.queenieho@gmail.com>
2020-12-22 16:41:01 -0800

    can also do a sort of the string first, then check if adjacent elements are the same

--- 12_22_2020a_uniqueChars.js
+++ 12_22_2020a_uniqueChars.js
@@ -1,4 +1,6 @@
 // An algorithm to determine if a string has all unique characters.
+// assume its an ASCII string instead of a unicode string
+
 let allUniqueChar = string => {
   let mySet = new Set(string)
   return string.length == mySet.size? true: false;
@@ -17,4 +19,16 @@ let allUniqueChar = string => {
 let allUniqueChar = string => {
   string = string.split('');
   return string.filter(char => string.indexOf(char) !== string.lastIndexOf(char)).length? false:true;
+}
+
+let isUniqueChars = string => {
+  //sort first
+  arr = string.split('').sort();
+  for(var i = 0; i < arr.length - 1; i++){
+    if(arr[i] == arr[i+1]){
+      return false;
+      break;
+    }
+  }
+  return true;
 }

67b7151d9a7fc120f72441ebbe502d99ee0c4212
Queenie Ho <qho.queenieho@gmail.com>
2021-01-06 22:05:11 -0800

    reverse just the vowels in the string...this is probably too much work since im trying to do it in place...anyways weird problem in the first place...?

---
+++ 1_6_2021b_reverseOnlyVowels_inPlace.js
@@ -0,0 +1,51 @@
+// Reverse Vowels of a String
+// Write a function that takes a string as input and reverse only the vowels of a string.
+//
+// Example 1:
+//
+// Input: "hello"
+// Output: "holle"
+// Example 2:
+//
+// Input: "leetcode"
+// Output: "leotcede"
+// Note:
+// The vowels does not include the letter "y".
+
+var reverseVowels = function(s) {
+    s = [...s];
+    let front = 0;
+    let back = s.length -1;
+    let frontVowelIndex = -1;
+    let backVowelIndex = -1;
+
+    let checkVowel = letter => {
+        let vowelPos = "aeiouAEIOU".indexOf(letter);
+        return vowelPos === -1? false:true;
+    }
+
+    while(front < back){
+        if(checkVowel(s[front])){
+            frontVowelIndex = front;
+        }
+        else{
+            front++;
+        }
+        if(checkVowel(s[back])){
+            backVowelIndex = back;
+        }
+        else{
+            back--;
+        }
+        if(frontVowelIndex !== -1 && backVowelIndex !== -1){
+            let temp = s[frontVowelIndex];
+            s.splice(frontVowelIndex, 1 , s[backVowelIndex])
+            s.splice(backVowelIndex, 1, temp)
+            front++;
+            back--;
+            frontVowelIndex = -1;
+            backVowelIndex = -1;
+        }
+    }
+    return s.join('');
+};

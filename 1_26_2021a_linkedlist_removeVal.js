e404ec1439160c5b4951543a061c4f4ed5c8fe53
Queenie Ho <qho.queenieho@gmail.com>
2021-01-27 17:34:52 -0800

    putting all non-val into a new linked list

--- 1_26_2021a_linkedlist_removeVal.js
+++ 1_26_2021a_linkedlist_removeVal.js
@@ -20,7 +20,24 @@ var removeElements = function(head, val) {
     return head.next;
 };
 
+// the difference from the previous solution is, less vars because we have replaced
+// previous with current.next.next, by checking current.next instead of current
+var removeElements = function(head, val) {
+    let newNode = new ListNode(null, head);
+    let current = newNode;
 
+    while(current && current.next){
+      if(current.next.val === val){
+        current.next = current.next.next;
+      }
+      else{
+        current = current.next;
+      }
+    }
+    return newNode.next;
+};
+
+// eliminates all possibilities that head can be val, check (always) check the 'next' one
 var removeElements = function(head, val) {
     while(head && head.val === val){
         head = head.next;
@@ -40,4 +57,21 @@ var removeElements = function(head, val) {
         }
     }
     return head;
+};
+
+// putting all non-val into a new linked list
+var removeElements = function(head, val) {
+    let newNode = new ListNode();
+    let newCurr = newNode;
+    let current = head;
+
+    while(current){
+        if(current.val !== val){
+            newCurr.next = current;
+            newCurr = newCurr.next;
+        }
+        current = current.next;
+    }
+    newCurr.next = null;
+    return newNode.next;
 };

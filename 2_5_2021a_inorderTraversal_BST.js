0b64817ec013fd93e36e959dd0b60f7064d6f1e4
Queenie Ho <qho.queenieho@gmail.com>
2021-02-05 19:35:49 -0800

    traverse a tree inroder, next is to use the stack method

---
+++ 2_5_2021a_inorderTraversal_BST.js
@@ -0,0 +1,24 @@
+// Binary Tree Inorder Traversal
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+// recursively:
+// keep going down to the last left, push to result, then the node itself, then the node's right
+var inorderTraversal = function(root) {
+    let result = [];
+    let helper = (root, result) => {
+        if(root !== null){
+            if(root.left !== null){
+                helper(root.left, result);
+            }
+            result.push(root.val);
+
+            if(root.right !== null){
+                helper(root.right, result);
+            }
+        }
+    }
+
+
+    helper(root, result);
+    return result;
+};

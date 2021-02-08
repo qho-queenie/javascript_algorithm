abe93c2d60c25d7ff080c45d7715b8f708610a77
Queenie Ho <qho.queenieho@gmail.com>
2021-02-07 20:11:56 -0800

    more methods for inorder traversal BST

--- 2_5_2021a_inorderTraversal_BST.js
+++ 2_5_2021a_inorderTraversal_BST.js
@@ -17,8 +17,25 @@ var inorderTraversal = function(root) {
             }
         }
     }
-
-
     helper(root, result);
+    return result;
+};
+
+
+var inorderTraversal = function(root) {
+    let result = [];
+    let stack = [];
+    let curr = root;
+
+    while(curr || stack.length){
+        while(curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        result.push(curr.val);
+        curr = curr.right;
+
+    }
     return result;
 };

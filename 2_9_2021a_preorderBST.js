0b08d9472fa15c2ab2a1d922e546e5621ebed654
Queenie Ho <qho.queenieho@gmail.com>
2021-02-09 17:08:37 -0800

    more ways to do preorder traversal

--- 2_9_2021a_preorderBST.js
+++ 2_9_2021a_preorderBST.js
@@ -2,6 +2,8 @@
 //
 // Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
+
+// push all current node, then if there is a node.left push it, with no node.left, push its right
 var preorderTraversal = function(root) {
     let result = [];
     let helper = (root,result) => {
@@ -15,3 +17,37 @@ var preorderTraversal = function(root) {
     helper(root, result);
     return result;
 };
+
+// same idea as above but with ES6
+var preorderTraversal = function(root) {
+    return root?[root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)] : [];
+};
+
+//using stack
+var preorderTraversal = function(root, stack = [], res = []) {
+    if (!root){
+       return [];
+    }
+    stack.push(root);
+    res.push(root.val);
+    if (root.left){
+        preorderTraversal(root.left, stack, res);
+    }
+    if (root.right){
+        preorderTraversal(root.right, stack, res);
+    }
+    return res;
+}
+
+// var preorderTraversal = function(root) {
+//     if (!root) return [];
+//     let stack = [], res = [];
+//     stack.push(root);
+//     while (stack.length) {
+//         let node = stack.pop();
+//         res.push(node.val);
+//         if (node.right) stack.push(node.right);
+//         if (node.left) stack.push(node.left);
+//     }
+//     return res;
+// };

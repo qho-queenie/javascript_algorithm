3c5cc63ee872426d82ee793dfd0635e8811aed67
Queenie Ho <qho.queenieho@gmail.com>
2021-02-11 15:07:00 -0800

    search value and return root

---
+++ 2_11_2021a_findValueBST.js
@@ -0,0 +1,26 @@
+// Search in a Binary Search Tree
+// You are given the root of a binary search tree (BST) and an integer val.
+//
+// Find the node in the BST that the node's value equals val and return the
+// subtree rooted with that node. If such a node does not exist, return null.
+//
+// Input: root = [4,2,7,1,3], val = 2
+// Output: [2,1,3]
+//
+// Input: root = [4,2,7,1,3], val = 5
+// Output: []
+
+var searchBST = function(root, val) {
+    if (root === null) {
+        return null;
+    }
+    if (val < root.val) {
+        return searchBST(root.left, val);
+    }
+    else if (val > root.val) {
+        return searchBST(root.right, val)
+    }
+    else {
+        return root;
+    }
+};

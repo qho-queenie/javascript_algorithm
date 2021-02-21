bdec0fad9cfdfb3a02e294b395e194228aa536a9
Queenie Ho <qho.queenieho@gmail.com>
2021-02-20 17:38:40 -0800

    slight improvement: instead of 3 nested loops, we use the complement of zero, and use 2-pointers technique to find the other 2 numbers in the array

--- 2_18_2021a_3sum.js
+++ 2_18_2021a_3sum.js
@@ -1,14 +1,15 @@
-3Sum
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
-Notice that the solution set must not contain duplicate triplets.
-
-Input: nums = [-1,0,1,2,-1,-4]
-Output: [[-1,-1,2],[-1,0,1]]
-
-
-Input: nums = [0]
-Output: []
+// 3Sum
+// Given an array nums of n integers, are there elements a, b, c in nums such
+// that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+//
+// Notice that the solution set must not contain duplicate triplets.
+//
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+//
+//
+// Input: nums = [0]
+// Output: []
 
 // very brute force, but for practicing the basics
 var threeSum = function(nums) {
@@ -16,7 +17,6 @@ var threeSum = function(nums) {
     for(var i = 0; i < nums.length - 2; i++){
         for(var j = i + 1; j < nums.length - 1; j++){
             for(var k = j + 1; k < nums.length; k++){
-                //console.log(nums[i], nums[j], nums[k])
                 if(nums[i] + nums[j] + nums[k] === 0){
                    result.push([nums[i], nums[j], nums[k]].sort((a,b) => a - b));
                 }
@@ -38,3 +38,22 @@ var threeSum = function(nums) {
 
     return multiDimensionalUnique(result);
 };
+
+
+var threeSum = function(nums) {
+    let target;
+    let targetArr;
+    let result = [];
+    nums = nums.sort((a,b) => a - b);
+    for(var i = 0; i < nums.length - 1; i++){
+        for(var j = i + 1; j < nums.length; j++){
+            targetArr = nums.slice(j+1);
+            target = targetArr.indexOf(0 - ((nums[i]) + (nums[j])));
+            if(target !== -1){
+                result.push([nums[i], nums[j], 0 - (nums[i] + nums[j])]);
+            }
+        }
+    }
+
+    return multiDimensionalUnique(result);
+}

111ac663a918e78079897d327d3dc85edaf6c607
Queenie Ho <qho.queenieho@gmail.com>
2021-02-22 00:48:44 -0800

    added comments for the bigO,

--- 2_21_2021a_groupAnagrams.js
+++ 2_21_2021a_groupAnagrams.js
@@ -6,6 +6,11 @@
 // An Anagram is a word or phrase formed by rearranging the letters of a different
 // word or phrase, typically using all the original letters exactly once.
 
+// Time Complexity: O(NK log K), where N is the length of strs, and K
+// is the maximum length of a string in strs. The outer loop has complexity O(N)
+// as we iterate through each string. Then, we sort each string in O(KlogK) time.
+// Space Complexity: O(NK), the total information content stored in ans
+
 var groupAnagrams = function(strs) {
     let sortedLetters = [];
     let strsSort = strs.forEach(x => sortedLetters.push([...x].sort()));
@@ -20,8 +25,15 @@ var groupAnagrams = function(strs) {
 };
 
 
-// Time Complexity: O(NK \log K)O(NKlogK), where NN is the length of strs, and KK 
-// is the maximum length of a string in strs. The outer loop has complexity O(N)O(N)
-// as we iterate through each string. Then, we sort each string in O(K \log K)O(KlogK) time.
-//
-// Space Complexity: O(NK)O(NK), the total information content stored in ans.
+
+// we dont need 2 loops since we dont wanna mess with the original order anyways
+// therefore we can sort each letter and put each of them in the hash table in the same loop
+var groupAnagrams = function(strs) {
+    let hash = {};
+    for(str of strs) {
+        let strsSort = str.split('').sort().join('');
+        hash[strsSort]? hash[strsSort].push(str):hash[strsSort] = [str];
+    }
+
+    return Object.values(hash);
+};

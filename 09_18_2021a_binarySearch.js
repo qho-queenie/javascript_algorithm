f08a77598eab1b0a07bdf40e7a5302f1f2a37502
Queenie Ho <qho.queenieho@gmail.com>
2021-09-21 11:45:07 -0700

    binary search for insert positiong as a redo, added notes for binary search

--- 09_18_2021a_binarySearch.js
+++ 09_18_2021a_binarySearch.js
@@ -4,6 +4,8 @@
 // repeat until midpoint is the target
 // if max is equal min, target doesnt exist
 
+// rule of thumb : exclude the mid since it has been proven target !== mid
+
 let search = (nums, target) => {
     let low = 0;
     let high = nums.length - 1;
@@ -42,5 +44,15 @@ let searchRecursive = (nums, target, low = 0, high = nums.length - 1) => {
     else if(nums[mid] < target){
         return search(nums, target, mid + 1, high);
     }
-
 };
+
+// https://leetcode.com/problems/binary-search/discuss/423162/Binary-Search-101
+//
+// My rule of thumb when it comes to binary search:
+//
+// Include ALL possible answers when initialize lo & hi
+// Don't overflow the mid calculation
+// Shrink boundary using a logic that will exclude mid
+// Avoid infinity loop by picking the correct mid and shrinking logic
+// Always think of the case when there are 2 elements left :
+  // imagine when there are only 2 elements left in the boundary. If the logic fell into the else statement, since we are using the left/lower mid, it's simply not doing anything. It just keeps shrinking itself to itself, and the program got stuck.
--- 8_24_2021a_searchInsertPos.js
+++ 8_24_2021a_searchInsertPos.js
@@ -1,11 +1,58 @@
 
-Search Insert Position
+// Search Insert Position
+//
+// Given a sorted array of distinct integers and a target value, return the index
+// if the target is found. If not, return the index where it would be if it were inserted in order.
 
-Given a sorted array of distinct integers and a target value, return the index
-if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
 
-You must write an algorithm with O(log n) runtime complexity.
+// this is nlogn binary search
+let binarySearchInsert = (nums, target) => {
+    let low = 0;
+    let high = nums.length - 1;
 
+    while(low < high){
+      let mid = low + Math.floor((high-low+1)/2);
+        if(target == nums[mid]){
+          return mid;
+        }
+        else if(nums[mid] > target) {
+            high = mid - 1;
+        }
+        else if(nums[mid] < target) {
+            low = mid + 1;
+        }
+    }
+
+    // not found in array, and finding where to insert the target
+    // high is equal to low
+    // if target is lower than low, directly replace low
+
+    if(nums[low] < target) return low + 1;
+    else {
+        if(low === 0) return 0;
+        else return low ;
+    }
+}
+
+// this is nlogn binary search
+let searchInsertRecursive = (nums, target, low = 0, hi = nums.length -1) => {
+    while(low < hi) {
+
+     let mid = low + Math.floor((hi-low+1)/2);
+        if(nums[mid] === target) return mid;
+        return target >  nums[mid] ? searchInsert(nums, target, mid + 1, hi) : searchInsert(nums, target, low, mid - 1);
+    }
+
+    // hi == low, meaning not found in array
+    if(nums[low] < target) return low + 1;
+    else {
+        if(low === 0) return 0;
+        else return low ;
+    }
+}
+
+// these are O(n) linear
 let searchInsert = (nums, target) => {
     let targetIndex = nums.indexOf(target);
     if(targetIndex > -1) {
@@ -18,6 +65,7 @@ let searchInsert = (nums, target) => {
     }
     return i;
 }
+
 
 let searchInsert = (nums, target) => {
     for(var i = 0; i < nums.length; i++) {
